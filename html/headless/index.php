0773bc8909663c3afd9f3d1beb71af5ba7d5d43b
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2019-01-23 20:38:50 +0100

    comments

--- html/headless/index.php
+++ html/headless/index.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Using WordPress as Headless CMS</title>
+  <title>The Headless Knight</title>
 </head>
 <body>
+
+  <!-- below: the javascript will write the content in #post-list -->
   <div id="posts-list"></div>
 
   <!-- polyfill -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.min.js" charset="utf-8"></script>
 
   <script type="text/javascript">
+    /*
+      Here is Uzayr's original script from the article.
+      It will print the post titles on the webpage.
+      By a similar methods you could write out post content, pages etc. etc.
+    */
     const pContain = document.getElementById('posts-list')
     fetch('http://localhost/wordpress/wp-json/wp/v2/posts')
-// response
+      // response
       .then(r => r.json())
       .then(posts => {
         posts.map(post => {
